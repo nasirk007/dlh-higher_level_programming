1d6b9c3c67b577f4526257866c453eab59fee531
Nasir9372 <nasirkhan9372@gmail.com>
2026-04-29 22:20:53 +0000

    corrections

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx):
     if idx < 0:
-        return None
+        return my_list
     elif idx >= len(my_list):
-        return None
+        return my_list
     else:
         return my_list[idx]
