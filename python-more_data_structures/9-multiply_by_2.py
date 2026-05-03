317fc08bd676d579a77d464909240e7d9b07ff08
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-03 21:38:07 +0000

    correction

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
-    for key, value in a a_dictionary.element():
+    for key, value in a a_dictionary.items():
         new_dictionary[key] = value * 2
         return new_dictionary
