3a40507081016b4ce894157a7b8c8174a7d764f4
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-10 17:08:38 +0000

    getter and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,25 +3,23 @@
 
 
 class Square:
-    """This is documentation of class Square by size and must be validated even after updated."""
+    """This is documentation of class Square by size."""
     def __init__(self, size=0):
+        self.__size = size
+
+    def getsize(self):
+        self.__size = size
+
+    def setsize(self, size):
+        return self.__size
+        "This is setter for size"
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size
 
     def area(self):
         area = self.__size * self.__size
-        return area 
-    
-    def size(self, value):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-
+        return area
