d92223bdf2a9592f61ea63c317489d4e2e5478ac
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-10 01:03:28 +0000

    task 4 with properties

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines the class Square."""
+
+
+class Square:
+    """This is documentation of class Square by size and must be validated even after updated."""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        area = self.__size * self.__size
+        return area 
+    
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
