5eb56ee15a2b778ba87e172a213c556055bcb38e
Nasir9372 <@nasirkhan9372@gmail.comgit config --global user.email nasirkhan9372@gmail.comgit config --global user.email @nasirkhan9372@gmail.com>
2026-05-13 08:36:03 +0000

    upper case, first loop over character in string, compare character in numeric form using ord() with ASCII, and if condition remain true convert numberic number back into character using chr()

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    ASCII = ord(ch)
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 123:
+            ch = chr(ord(ch) - 32)
+            print("{}.format(ch))
