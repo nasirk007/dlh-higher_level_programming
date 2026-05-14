4609a9f5e05cf8aa023cfd9416483136538af400
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-14 22:28:36 +0000

    mathametical operations

---
+++ python-import_modules/1-calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from add_0 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
