40aa28854da0ce2fed9e958504ca27d290d36ef0
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-16 16:36:42 +0000

    addition of argument after converting them into integer

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__"
-print("{}".format())
+# execute the code if this file is a main or just like admin"
+if __name__ == "__main__":
+    # define what is argument and from where it will come from, user or system
+    argument = sys.argv
+    # store result of each addition after converting each argument into integer
+    total = 0
+    # indexing will start from 1 as sys.argv itself is first argument in list
+    for i in range(1, len(sys.argv)):
+        # sys.argv[i] will return value instead of integer
+        total += int(sys.argv[i])
+    print(total)
